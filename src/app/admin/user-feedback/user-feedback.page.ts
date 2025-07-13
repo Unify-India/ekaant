e0e7f0cbae3e7aba11ff9397cce42d73909da620
IAM5K <sandeep2k2656@gmail.com>
2025-07-14 02:51:11 +0530

    init: create user feedback page

---
+++ src/app/admin/user-feedback/user-feedback.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-user-feedback',
+  templateUrl: './user-feedback.page.html',
+  styleUrls: ['./user-feedback.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class UserFeedbackPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
