24d2957ee2b8cbbd46fe33b6c5c636aceda1d440
IAM5K <sandeep2k2656@gmail.com>
2025-07-14 02:50:55 +0530

    init: create student applications detail page for manager

---
+++ src/app/manager/student-application-detail/student-application-detail.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { StudentApplicationDetailPage } from './student-application-detail.page';
+
+describe('StudentApplicationDetailPage', () => {
+  let component: StudentApplicationDetailPage;
+  let fixture: ComponentFixture<StudentApplicationDetailPage>;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StudentApplicationDetailPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/manager/student-application-detail/student-application-detail.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-student-application-detail',
+  templateUrl: './student-application-detail.page.html',
+  styleUrls: ['./student-application-detail.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class StudentApplicationDetailPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
