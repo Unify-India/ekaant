186aedaf1a45864f3ad372baef460d4802e256eb
IAM5K <sandeep2k2656@gmail.com>
2025-07-14 02:45:15 +0530

    init: create apply for library page

---
+++ src/app/student/apply-library/apply-library.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ApplyLibraryPage } from './apply-library.page';
+
+describe('ApplyLibraryPage', () => {
+  let component: ApplyLibraryPage;
+  let fixture: ComponentFixture<ApplyLibraryPage>;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ApplyLibraryPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/student/apply-library/apply-library.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-apply-library',
+  templateUrl: './apply-library.page.html',
+  styleUrls: ['./apply-library.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class ApplyLibraryPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
