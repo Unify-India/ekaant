28e11509f6f66f65cca3bc5aebbc1d74efb6ce64
IAM5K <sandeep2k2656@gmail.com>
2025-07-14 02:52:19 +0530

    init: create library request page for admin

---
+++ src/app/admin/library-requests/library-requests.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LibraryRequestsPage } from './library-requests.page';
+
+describe('LibraryRequestsPage', () => {
+  let component: LibraryRequestsPage;
+  let fixture: ComponentFixture<LibraryRequestsPage>;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LibraryRequestsPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
