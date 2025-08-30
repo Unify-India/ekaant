1abeaf95993556b1d4aab063c9641222d8cb0eb9
IAM5K <sandeep2k2656@gmail.com>
2025-08-30 11:40:20 +0530

    feat: add toaster

---
+++ src/app/services/toaster/toaster.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ToasterService {
+  constructor(private toastController: ToastController) {}
+
+  async showToast(message: string, color: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 3000,
+      color: color,
+    });
+    toast.present();
+  }
+}
