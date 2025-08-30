a40fc0ea6afc8a2a36a45afd77b9875a5af0c0dd
IAM5K <sandeep2k2656@gmail.com>
2025-08-30 11:27:15 +0530

    feat: add auth guards

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,22 @@
+import { inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import {
+  Router,
+  CanActivateFn,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
+
+export const authGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+  if (auth.isLoggedIn()) {
+    return true;
+  } else {
+    router.navigate(['/login']);
+    return false;
+  }
+};
