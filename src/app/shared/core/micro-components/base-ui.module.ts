67ea1e9bfcfc471a81d6ccb4691c414172875575
IAM5K <sandeep2k2656@gmail.com>
2025-08-30 11:48:55 +0530

    feat: create common modules for base ui

---
+++ src/app/shared/core/micro-components/base-ui.module.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import {
+  IonHeader,
+  IonButton,
+  IonToolbar,
+  IonButtons,
+  IonMenu,
+  IonMenuButton,
+  IonTitle,
+  IonContent,
+} from '@ionic/angular/standalone';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonButton,
+    IonButtons,
+    IonMenuButton,
+    IonMenu,
+    IonTitle,
+    IonContent,
+    IonToolbar,
+  ],
+  exports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonButton,
+    IonButtons,
+    IonMenuButton,
+    IonMenu,
+    IonTitle,
+    IonContent,
+    IonToolbar,
+  ],
+})
+export class BaseUiComponents {}
