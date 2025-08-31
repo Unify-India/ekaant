0ab172f41fd55ffc47f12b9537a40a175743dc05
IAM5K <sandeep2k2656@gmail.com>
2025-08-31 07:34:04 +0530

    feat: update admin module

---
+++ src/app/admin/admin-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./dashboard/dashboard.page').then((m) => m.DashboardPage),
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AdminRoutingModule {}
