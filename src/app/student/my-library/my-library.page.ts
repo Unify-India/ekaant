c0f1209dc1e0c21a8ffab0119584bf14a3d70924
IAM5K <sandeep2k2656@gmail.com>
2025-08-31 16:26:17 +0530

    feat: add my library page for student

---
+++ src/app/student/my-library/my-library.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-my-library',
+  templateUrl: './my-library.page.html',
+  styleUrls: ['./my-library.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class MyLibraryPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
