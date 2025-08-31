4d62a149db967e7941d6df342e69af3537c458dd
IAM5K <sandeep2k2656@gmail.com>
2025-08-31 15:02:26 +0530

    feat: add services, testimonials and footer to home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,17 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonIcon } from '@ionic/angular/standalone';
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonIcon,
+  IonCol,
+  IonRow,
+  IonGrid,
+} from '@ionic/angular/standalone';
+import { alertCircleOutline, calendarOutline, locationOutline } from 'ionicons/icons';
 import { BaseUiComponents } from 'src/app/shared/core/micro-components/base-ui.module';
+import { addIcons } from 'ionicons';
+import { FooterPage } from '../footer/footer.page';
+import { FeedbackPage } from '../feedback/feedback.page';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [BaseUiComponents, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonIcon, FormsModule],
+  imports: [
+    IonGrid,
+    IonRow,
+    IonCol,
+    BaseUiComponents,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonIcon,
+    FormsModule,
+    FeedbackPage,
+    FooterPage,
+  ],
+  providers: [],
 })
 export class HomePage implements OnInit {
   pageTitle = 'Welcome to Ekaant';
+  icons = { calendarOutline, alertCircleOutline, locationOutline };
   studentsFeatures = [
     {
       title: 'Easy Digital Enrollment',
@@ -47,7 +75,7 @@ export class HomePage implements OnInit {
       description: `Enjoy full access to all features of the Study Portal 
         with no charges for 6 months. Perfect for owners to experience 
         the benefits of a smarter digital system replacing traditional registers.`,
-      price: '$0',
+      price: '₹0',
       duration: '',
       buttonText: 'Start Free Trial',
       buttonColor: 'success',
@@ -59,8 +87,8 @@ export class HomePage implements OnInit {
       description: `Access to essential study room management features including 
         easy digital enrollment, real-time seat availability, and basic support ticketing. 
         Ideal for small to medium-sized libraries.`,
-      price: '$5',
-      duration: '/month',
+      price: '₹5',
+      duration: '/month/student',
       buttonText: 'Get Started',
       buttonColor: 'primary',
       image: 'https://picsum.photos/seed/standard/400/200',
@@ -71,15 +99,17 @@ export class HomePage implements OnInit {
       description: `Full-featured subscription including all Standard Plan benefits plus 
         streamlined record-keeping, automatic revenue tracking, priority support, and 
         enhanced student satisfaction tools. Best for larger institutions.`,
-      price: '$15',
-      duration: '/month',
+      price: '₹15',
+      duration: '/month/student',
       buttonText: 'Go Premium',
       buttonColor: 'dark',
       image: 'https://picsum.photos/seed/premium/400/200',
       popular: false,
     },
   ];
-  constructor() {}
+  constructor() {
+    addIcons(this.icons);
+  }
 
   ngOnInit(): void {
     // Initialization logic can go here
