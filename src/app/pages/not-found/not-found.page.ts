779ee8c0451e061aec97bc178d69def2112f746f
IAM5K <sandeep2k2656@gmail.com>
2025-08-31 16:26:40 +0530

    feat: add page not found with auto re-routing

--- src/app/pages/not-found/not-found.page.ts
+++ src/app/pages/not-found/not-found.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-not-found',
@@ -11,7 +13,50 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule],
 })
 export class NotFoundPage implements OnInit {
-  constructor() {}
+  counter = 5;
+  private intervalId: any;
+  private routerSub!: Subscription;
 
-  ngOnInit() {}
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Subscribe to navigation events to reset countdown if we land here again
+    this.routerSub = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Only reset if we are still on the not-found page
+        if (this.router.url && this.router.url !== '/home') {
+          this.startCountdown();
+        }
+      }
+    });
+
+    this.startCountdown();
+  }
+
+  private startCountdown() {
+    // Clear previous timer if any
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+
+    this.counter = 5;
+
+    this.intervalId = setInterval(() => {
+      this.counter--;
+
+      if (this.counter === 0) {
+        clearInterval(this.intervalId);
+        this.router.navigate(['/home']);
+      }
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+    if (this.routerSub) {
+      this.routerSub.unsubscribe();
+    }
+  }
 }
