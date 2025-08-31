76df16321d19e5133f4cdd294bf2910820574ff5
IAM5K <sandeep2k2656@gmail.com>
2025-08-31 14:58:43 +0530

    feat: add footer

--- src/app/pages/footer/footer.page.ts
+++ src/app/pages/footer/footer.page.ts
@@ -1,20 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonFooter } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.page.html',
   styleUrls: ['./footer.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonFooter, IonToolbar, CommonModule, FormsModule],
 })
 export class FooterPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
