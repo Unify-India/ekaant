7423e3848a603a2259ea0014babf5f6c4c17d7b1
IAM5K <sandeep2k2656@gmail.com>
2025-09-06 16:44:57 +0530

    feat: add cubicle card for reuse and dynamic rendering

---
+++ src/app/components/cubicle-card/cubicle-card.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { CubicleCardComponent } from './cubicle-card.component';
+
+describe('CubicleCardComponent', () => {
+  let component: CubicleCardComponent;
+  let fixture: ComponentFixture<CubicleCardComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CubicleCardComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CubicleCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/cubicle-card/cubicle-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cubicle-card',
+  templateUrl: './cubicle-card.component.html',
+  styleUrls: ['./cubicle-card.component.scss'],
+})
+export class CubicleCardComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
