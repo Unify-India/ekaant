5e792a2230e8d7b4b47765e3215a73090d13c9cb
IAM5K <sandeep2k2656@gmail.com>
2025-09-06 16:46:31 +0530

    feat: add users page to show all related user

---
+++ src/app/manager/users/users.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-enrolled-users',
+  templateUrl: './users.page.html',
+  styleUrls: ['./users.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule],
+})
+export class UsersPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
