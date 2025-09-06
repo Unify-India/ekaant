10b4d3f625a57442cc7d3c2c4106c671d2bdb741
IAM5K <sandeep2k2656@gmail.com>
2025-09-06 16:46:08 +0530

    feat: add campaign page to show all options

---
+++ src/app/manager/campaign/campaign.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-campaign',
+  templateUrl: './campaign.page.html',
+  styleUrls: ['./campaign.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class CampaignPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
