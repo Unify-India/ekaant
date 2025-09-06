e82648c346c1bf75c522055d5a7b4cad96d75c82
IAM5K <sandeep2k2656@gmail.com>
2025-09-06 17:25:52 +0530

    feat: add landing screen banner

---
+++ src/app/pages/home/landing-screen/landing-screen.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { IonButton } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-landing-screen',
+  templateUrl: './landing-screen.component.html',
+  styleUrls: ['./landing-screen.component.scss'],
+  standalone: true,
+  imports: [IonButton, RouterLink],
+})
+export class LandingScreenComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
