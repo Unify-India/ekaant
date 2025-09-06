a9c69a3aabbe72485837709a1796bf55e99d0237
IAM5K <sandeep2k2656@gmail.com>
2025-09-06 16:44:38 +0530

    feat: add study session for reuse and dynamic rendering

---
+++ src/app/components/study-sessions/study-sessions.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { StudySessionsComponent } from './study-sessions.component';
+
+describe('StudySessionsComponent', () => {
+  let component: StudySessionsComponent;
+  let fixture: ComponentFixture<StudySessionsComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ StudySessionsComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(StudySessionsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/study-sessions/study-sessions.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-study-sessions',
+  templateUrl: './study-sessions.component.html',
+  styleUrls: ['./study-sessions.component.scss'],
+})
+export class StudySessionsComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
