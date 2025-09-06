db7be2abf5b15c4d845dbd623d7ea8adc5b51baf
IAM5K <sandeep2k2656@gmail.com>
2025-09-06 16:48:18 +0530

    feat: add slot management page to manage availability

---
+++ src/app/manager/slot-management/slot-management.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SlotManagementPage } from './slot-management.page';
+
+describe('SlotManagementPage', () => {
+  let component: SlotManagementPage;
+  let fixture: ComponentFixture<SlotManagementPage>;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SlotManagementPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/manager/slot-management/slot-management.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-slot-management',
+  templateUrl: './slot-management.page.html',
+  styleUrls: ['./slot-management.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class SlotManagementPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
