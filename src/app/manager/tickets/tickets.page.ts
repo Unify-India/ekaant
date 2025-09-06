efe536d7b62c432a4d55f3f07267983a9c7d3fd2
IAM5K <sandeep2k2656@gmail.com>
2025-09-06 16:47:27 +0530

    feat: add tickets page to show all reported tickets

---
+++ src/app/manager/tickets/tickets.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-tickets-board',
+  templateUrl: './tickets.page.html',
+  styleUrls: ['./tickets.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule],
+})
+export class TicketsPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
