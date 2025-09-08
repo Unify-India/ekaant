830f2ce442da0832f1879ebb0cd16c42a15445c6
IAM5K <sandeep2k2656@gmail.com>
2025-09-08 17:58:10 +0530

    feat: create code of conduct section for library registration

---
+++ src/app/pages/library-registration-form/components/code-of-conduct/code-of-conduct.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { CodeOfConductComponent } from './code-of-conduct.component';
+
+describe('CodeOfConductComponent', () => {
+  let component: CodeOfConductComponent;
+  let fixture: ComponentFixture<CodeOfConductComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CodeOfConductComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CodeOfConductComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
