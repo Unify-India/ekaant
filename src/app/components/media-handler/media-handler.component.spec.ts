2e485091f3a3fed354a2b796a32e7bf9bbe47e82
IAM5K <sandeep2k2656@gmail.com>
2025-10-02 11:09:29 +0530

    feat: add media handler component for common file upload

---
+++ src/app/components/media-handler/media-handler.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { MediaHandlerComponent } from './media-handler.component';
+
+describe('MediaHandlerComponent', () => {
+  let component: MediaHandlerComponent;
+  let fixture: ComponentFixture<MediaHandlerComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MediaHandlerComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MediaHandlerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/media-handler/media-handler.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-media-handler',
+  templateUrl: './media-handler.component.html',
+  styleUrls: ['./media-handler.component.scss'],
+})
+export class MediaHandlerComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
