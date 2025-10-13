21a902137f2cdc4cffee99996b65fdba694f3611
IAM5K <sandeep2k2656@gmail.com>
2025-10-13 08:34:49 +0530

    feat: add student profile page

--- src/app/student/profile/profile.page.ts
+++ src/app/student/profile/profile.page.ts
@@ -1,17 +1,28 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { personCircle, personOutline, cloudUploadOutline, saveOutline, personCircleOutline } from 'ionicons/icons';
+import { BaseUiComponents } from 'src/app/shared/core/micro-components/base-ui.module';
+import { FormEssentials } from 'src/app/shared/core/micro-components/form-essentials.module';
+import { UiEssentials } from 'src/app/shared/core/micro-components/ui-essentials.module';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule],
+  imports: [BaseUiComponents, FormEssentials, UiEssentials],
 })
 export class ProfilePage implements OnInit {
-  constructor() {}
+  public selectedFacility = 'ac';
+  pageTitle = 'Profile';
+
+  constructor() {
+    addIcons({ personCircleOutline, personOutline, cloudUploadOutline, saveOutline, personCircle });
+  }
+
+  saveProfile() {
+    console.log('Profile saved!');
+  }
 
   ngOnInit() {}
 }
