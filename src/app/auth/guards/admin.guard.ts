2277e2aa2f83a3a3f059836610a2ffdbf1a4cfc7
IAM5K <sandeep2k2656@gmail.com>
2025-10-13 08:04:39 +0530

    refactor: update path for auth service

--- src/app/auth/guards/admin.guard.ts
+++ src/app/auth/guards/admin.guard.ts
@@ -1,7 +1,7 @@
 import { inject } from '@angular/core';
 import { Router, CanActivateFn, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
-import { AuthService } from '../../services/auth.service';
+import { AuthService } from '../service/auth.service';
 
 export const adminGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
   const auth = inject(AuthService);
