e9f3731f185694cd08746f53079f8bcd5e8708b5
IAM5K <sandeep2k2656@gmail.com>
2025-11-09 23:02:51 +0530

    feat: add campaign page for managers

--- src/app/manager/campaign/campaign.page.ts
+++ src/app/manager/campaign/campaign.page.ts
@@ -1,17 +1,71 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonToggle } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  analyticsOutline,
+  buildOutline,
+  downloadOutline,
+  headsetOutline,
+  heartOutline,
+  peopleOutline,
+  ribbonOutline,
+  trendingUpOutline,
+} from 'ionicons/icons';
+import { BaseUiComponents } from 'src/app/shared/core/micro-components/base-ui.module';
+import { UiEssentials } from 'src/app/shared/core/micro-components/ui-essentials.module';
 
 @Component({
   selector: 'app-campaign',
   templateUrl: './campaign.page.html',
   styleUrls: ['./campaign.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule],
+  imports: [IonToggle, BaseUiComponents, UiEssentials, IonToggle, FormsModule],
 })
 export class CampaignPage implements OnInit {
-  constructor() {}
+  campaigns = [
+    {
+      name: 'Sponsored Listing',
+      description: 'Appear at the top of search results',
+      price: 29,
+      benefit: '+40% visibility boost',
+      active: false,
+      colorClass: 'campaign-blue',
+    },
+    {
+      name: 'Top Listed',
+      description: 'Premium placement in library listings',
+      price: 19,
+      benefit: 'Premium badge included',
+      active: false,
+      colorClass: 'campaign-green',
+    },
+    {
+      name: 'Featured Library',
+      description: 'Homepage spotlight & social promotion',
+      price: 49,
+      benefit: 'Maximum exposure package',
+      active: false,
+      colorClass: 'campaign-orange',
+    },
+  ];
+
+  constructor() {
+    addIcons({
+      heartOutline,
+      downloadOutline,
+      trendingUpOutline,
+      peopleOutline,
+      ribbonOutline,
+      buildOutline,
+      analyticsOutline,
+      headsetOutline,
+    });
+  }
 
   ngOnInit() {}
+
+  toggleCampaign(campaign: any) {
+    campaign.active = !campaign.active;
+  }
 }
